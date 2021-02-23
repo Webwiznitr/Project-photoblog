af57d012f2c6cb1c7bae73354110285cd55b397b
Sonakshi1901 <sonakshi1901@gmail.com>
2021-02-23 16:12:04 +0400

    Create Home.js

---
+++ frontend/frontend/src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+import PhotoCard from "../PhotoCard/PhotoCard";
+import SignIn from "../Register/SignIn/SignIn";
+import SignUp from "../Register/SignUp/SignUp";
+
+const Home = () => {
+
+  return (
+    <div className="home">
+      <div className="home__header">
+        <h2>PhotoBlog</h2>
+          <div className="home__post">
+            <SignUp/>
+            {/*<SignIn/>*/}
+            {/*<PhotoCard name= "username" date= "FEB 3, 2021" title= "A cat" caption= "This is so cool!!!"/>*/}
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
